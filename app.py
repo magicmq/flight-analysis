fbc6d7af451a3c7019d4b78934e0b4f5cf5cb88e
Michael <mquindt@yahoo.com>
2024-11-15 19:38:30 -0600

    Use MySQL for data retrieval

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import dash_mantine_components as dmc
 
 from layout import get_layout
 import callbacks
+from data import init_cache
 
 _dash_renderer._set_react_version('18.2.0')
 
 app = Dash(__name__, external_stylesheets=dmc.styles.ALL, suppress_callback_exceptions=True)
+
+init_cache(app)
+
 app.layout = get_layout()
 
 callbacks.init_callbacks(app)
--- data.py
+++ data.py
@@ -1,65 +1,89 @@
-import sqlite3
+from sqlalchemy import create_engine, MetaData, Table
 import pandas as pd
 import calendar
+import os
+
+from flask_caching import Cache
+
+from dotenv import load_dotenv
+
+load_dotenv()
+MYSQL_IP = os.getenv('MYSQL_IP')
+MYSQL_PORT = os.getenv('MYSQL_PORT')
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+
+cache = None
+
+def init_cache(app):
+    global cache
+
+    cache = Cache(app.server, config={
+        "CACHE_TYPE": "SimpleCache",
+        "CACHE_DEFAULT_TIMEOUT": 21600
+    })
 
 def fetch_data():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    @cache.memoize()
+    def _fetch_data():
+        engine = create_engine(f'mysql+mysqldb://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_IP}:{MYSQL_PORT}/data')
+        metadata = MetaData()
 
-    left_table = 'data'
-    right_table = 'data_post'
-    exclude_left_columns = ['flight_raw', 'pass_rider_raw']
-    exclude_right_columns = ['p_data_raw']
-    join_column = 'hash'
+        left_table = Table('data', metadata, autoload_with=engine)
+        right_table = Table('data_post', metadata, autoload_with=engine)
 
-    cursor.execute(f'PRAGMA table_info({left_table})')
-    left_columns = [col[1] for col in cursor.fetchall()]
+        exclude_left_columns = ['flight_raw', 'pass_rider_raw']
+        exclude_right_columns = ['p_data_raw']
+        join_column = 'hash'
 
-    cursor.execute(f'PRAGMA table_info({right_table})')
-    right_columns = [col[1] for col in cursor.fetchall()]
+        left_columns = [col.name for col in left_table.columns if col.name not in exclude_left_columns]
+        right_columns = [
+            col.name for col in right_table.columns
+            if col.name not in exclude_right_columns and col.name not in left_columns and col.name != join_column
+        ]
 
-    filtered_left_columns = [col for col in left_columns if col not in exclude_left_columns]
-    filtered_right_columns = [col for col in right_columns if col not in exclude_right_columns and col not in left_columns and col != join_column]
+        left_columns_str = ", ".join([f'data.{col}' for col in left_columns])
+        right_columns_str = ", ".join([f'data_post.{col}' for col in right_columns])
 
-    left_columns_str = ", ".join([f'{left_table}.{col}' for col in filtered_left_columns])
-    right_columns_str = ", ".join([f'{right_table}.{col}' for col in filtered_right_columns])
+        query = f"""
+            SELECT {left_columns_str}, {right_columns_str}
+            FROM data
+            JOIN data_post ON data.{join_column} = data_post.{join_column}
+        """
 
-    query = f"""
-        SELECT {left_columns_str}, {right_columns_str}
-        FROM {left_table}
-        JOIN {right_table} ON {left_table}.{join_column} = {right_table}.{join_column}
-    """
+        with engine.connect() as connection:
+            data = pd.read_sql_query(query, connection)
+        
+        return data
 
-    data = pd.read_sql_query(query, connection)
-
-    connection.close()
-
-    return data
+    return _fetch_data()
 
 def transform_data(data):
-    data['flight_no'] = data['flight_no'].str.lstrip('0')
+    @cache.memoize()
+    def _transform_data(_data):
+        _data['flight_no'] = _data['flight_no'].str.lstrip('0')
 
-    data['date'] = pd.to_datetime(data['date'])
+        _data['date'] = pd.to_datetime(_data['date'])
 
-    data['route'] = data.apply(lambda row: f'UAL{row["flight_no"]} ({row["origin"]}-{row["destination"]})', axis=1)
+        _data['route'] = _data.apply(lambda row: f'UAL{row["flight_no"]} ({row["origin"]}-{row["destination"]})', axis=1)
 
-    day_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    data['day_of_week_name'] = data['day_of_week'].map({i: day for i, day in enumerate(calendar.day_name)})
-    data['day_of_week_name'] = pd.Categorical(data['day_of_week_name'], categories=day_order, ordered=True)
+        day_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        _data['day_of_week_name'] = _data['day_of_week'].map({i: day for i, day in enumerate(calendar.day_name)})
+        _data['day_of_week_name'] = pd.Categorical(_data['day_of_week_name'], categories=day_order, ordered=True)
 
-    data['p_av_bu'] = data['p_ca_bu'] - data['p_bo_bu'] - data['p_ps_bu'] - data['p_he_bu'] - data['p_re_bu']
-    data['p_av_co'] = data['p_ca_co'] - data['p_bo_co'] - data['p_ps_co'] - data['p_he_co'] - data['p_re_co']
-    data['p_av_pp'] = data['p_ca_pp'] - data['p_bo_pp'] - data['p_ps_pp'] - data['p_he_pp'] - data['p_re_pp']
-    data['p_av_to'] = data['p_ca_to'] - data['p_bo_to'] - data['p_ps_to'] - data['p_he_to'] - data['p_re_to']
+        _data['p_av_bu'] = _data['p_ca_bu'] - _data['p_bo_bu'] - _data['p_ps_bu'] - _data['p_he_bu'] - _data['p_re_bu']
+        _data['p_av_co'] = _data['p_ca_co'] - _data['p_bo_co'] - _data['p_ps_co'] - _data['p_he_co'] - _data['p_re_co']
+        _data['p_av_pp'] = _data['p_ca_pp'] - _data['p_bo_pp'] - _data['p_ps_pp'] - _data['p_he_pp'] - _data['p_re_pp']
+        _data['p_av_to'] = _data['p_ca_to'] - _data['p_bo_to'] - _data['p_ps_to'] - _data['p_he_to'] - _data['p_re_to']
 
-    data['net_pos_va'] = data['av_to'] - data['pos_va']
-    data['net_pos_pe'] = data['av_to'] - data['pos_pe']
+        _data['net_pos_va'] = _data['av_to'] - _data['pos_va']
+        _data['net_pos_pe'] = _data['av_to'] - _data['pos_pe']
 
-    return data
+    return _transform_data(data)
 
 def get_data():
     data = fetch_data()
-    data = transform_data(data)
+    transform_data(data)
     return data
 
 def get_data_as(type):
