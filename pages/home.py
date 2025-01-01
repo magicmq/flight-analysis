7abc8a57b474c30a508cbe20edc857af31164fa7
Michael Q. <mquindt@yahoo.com>
2025-01-01 17:46:14 -0600

    Remove flight modal

--- pages/home.py
+++ pages/home.py
@@ -4,14 +4,12 @@ import dash_mantine_components as dmc
 import dash_new_tab as dnt
 
 from components.appshell import construct_appshell
-from components.flightmodal import construct_flight_modal
 
 dash.register_page(__name__, title='Flight Analysis', path='/')
 
 layout = html.Div([
     construct_appshell(),
     dmc.NotificationProvider(position='bottom-left'),
-    construct_flight_modal(),
     html.Div(id='notifications-container'),
     dcc.Download(id='download-data'),
     dnt.NewTab(id='new-tab-handler', n_opens=0),
